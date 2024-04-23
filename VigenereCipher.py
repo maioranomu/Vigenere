1c86c3cfef53c79335d79ab2c7808e1eb4e3d5af
Murillo Maiorano <79369489+maioranomu@users.noreply.github.com>
2024-04-23 10:34:15 -0300

    Update VigenereCipher.py

--- VigenereCipher.py
+++ VigenereCipher.py
@@ -1,4 +1,3 @@
-from calendar import c
 import os, time
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 detail = False
